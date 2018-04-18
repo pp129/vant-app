985c08264019a5db17f0621049277e2fb6e1f91f
PanFei <604042454@qq.com>
2018-04-19 00:18:50 +0800

    Initial commit

---
+++ src/vue/components/Tabbar/index.vue
@@ -0,0 +1,71 @@
+<template>
+	<van-tabbar v-model="active" style="z-index: 1999">
+		<van-tabbar-item 
+			v-for="(tab, index) in tabbar" 
+			:icon="tab.icon" 
+			:to="tab.path"
+			:dot="tab.dot"
+			:info="tab.info"
+			:key="index">
+			{{tab.name}}
+		</van-tabbar-item>
+	</van-tabbar>	
+</template>
+
+
+<script>
+	import { Tabbar, TabbarItem } from 'vant';
+	export default {
+		data() {
+			return {
+				active: 0,
+				tabbar: [{
+						name: "主页",
+						path: "/home",
+						pathName: "home",
+						icon: "like",
+						dot: false,
+						info: ""
+					},
+					{
+						name: "我的",
+						path: "/user",
+						pathName: "user",
+						icon: "wode",
+						dot: false,
+						info: ""
+					}
+				]
+			}
+		},
+		
+		watch:{
+			'$route': 'changeActive'
+		},
+		
+		created(){
+			const toName = this.$route.name
+			this.setActive(toName)
+		},
+		
+		methods: {
+			changeActive({ name }){
+				this.setActive(name)
+			},
+			setActive(name){
+				this.tabbar.forEach((tab, i) => {
+					if(tab.pathName == name){
+						this.active = i;
+						return false;
+					}
+				})
+			}
+		},
+		
+		components:{
+			 [Tabbar.name]: Tabbar,
+			 [TabbarItem.name]: TabbarItem,
+		}
+	}
+
+</script>
---
+++ src/vue/router/index.js
@@ -0,0 +1,41 @@
+import Vue from 'vue';
+import {
+	Toast
+} from 'vant';
+import Router from 'vue-router';
+
+import home from './home';
+import items from './items';
+import user from './user';
+import order from './order';
+import login from './login';
+
+Vue.use(Router);
+
+let RouterModel = new Router({
+	routes: [...home, ...items, ...user, ...order, ...login]
+})
+
+//路由拦截 判断登录状态
+RouterModel.beforeEach((to, from, next) => {
+	const {
+		Authorization,
+		user_id
+	} = Vue.prototype.$util.getLocalStorage('Authorization', 'user_id')
+	if (!Authorization && !user_id) {
+		if (to.meta.login) {
+			RouterModel.push({
+				name: 'login',
+				query: {
+					redirect: to.name
+				}
+			});
+			return;
+		}
+	}
+	next();
+})
+
+RouterModel.afterEach((to, from) => {})
+
+export default RouterModel;
